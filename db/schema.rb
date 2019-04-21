420015a965465af4a3c0d09716adda59c2c219de
Brandon Teich <brandonteich123@gmail.com>
2019-04-21 22:23:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
  ActiveRecord::Schema.define(version: 1) do
-
    create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
  end
