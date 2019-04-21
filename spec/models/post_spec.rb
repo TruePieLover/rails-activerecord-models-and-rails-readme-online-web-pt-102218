3bd9c5a48c87a3238eb09efcf8758b9cac26f5bf
Brandon Teich <brandonteich123@gmail.com>
2019-04-21 22:24:39 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,4 +9,5 @@ require 'rails_helper'
     post = Post.create!(title: "My title", description: "The post description")
     expect(post.post_summary).to eq("My title - The post description")
   end
+  
 end 
